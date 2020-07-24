023661fe511256f3ac8105f85b097183920e66f1
Jonathan Vice <jajvice@gmail.com>
2020-07-24 15:58:27 +0200

    Add one pass mode

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ document.onreadystatechange = () => {
         return +(Math.round(num + "e+" + places) + "e-" + places);
     }
 
+    function getStartLevel(endLevel, stage, totalPasses, usePremium, useCracker) {
+        let level = endLevel
+        let remainingPasses = totalPasses;
+        let remainingHours = 0;
+
+        while (remainingPasses > 0 || remainingHours > 12) {
+            if (remainingHours == 0) {
+                remainingPasses -= Math.max(stage, 1);
+                remainingHours = 12;
+            }
+
+            level -= getExpPerHour(level, usePremium, useCracker);
+            remainingHours -= 1;
+        }
+
+        return roundTo(level, 4);
+    }
+
     function getExpPerHour(level, usePremium, useCracker) {
         let exp = 0
 
@@ -33,25 +51,44 @@ document.onreadystatechange = () => {
                 remainingHours = 12;
             }
 
-            const exp = getExpPerHour(level, usePremium, useCracker);
-            level = level + exp;
+            level = level + getExpPerHour(level, usePremium, useCracker);
             remainingHours -= 1;
         }
 
-        return { totalPasses: totalPasses, level: roundTo(level, 4) };
+        return { totalPasses: totalPasses, level: roundTo(level, 5) };
     }
 
-    document.getElementById("petDetails").onsubmit = (e) => {
-        e.preventDefault();
+    function calculate() {
         const startLvl = Number(document.getElementById("startLvl").value);
         const endLvl = Number(document.getElementById("endLvl").value);
         const stage = Number(document.getElementById("stage").value);
         const usePremium = Boolean(document.getElementById("premium").checked);
         const useCracker = Boolean(document.getElementById("cracker").checked);
         const passType = (usePremium === true) ? " premium" : " normal";
-        const ender = (useCracker === true) ? " using crackers" : "";
+        let ender = (useCracker === true) ? " using crackers" : "";
         const result = calcPasses(startLvl, endLvl, stage, usePremium, useCracker);
-        const out = "It would take " + result.totalPasses + passType + " passes (" + result.totalPasses / Math.max(1, stage) * 12 + " hours) to level your s" + stage + " pet from " + startLvl + " to " + result.level + ender;
+        const plural = (result.totalPasses > 1) ? "es" : "";
+
+        // TODO
+        // if end exp is greater than 4 * exp_per_hour then it should be pretty
+        // easy to save one pass
+        // const hintCheck = result.level - Math.trunc(result.level) >= 4 * getExpPerHour(result.level, usePremium, useCracker);
+        // const hintLvl = getStartLevel(endLvl, stage, result.totalPasses, usePremium, useCracker);
+        // if (hintCheck === true) ender += "<br><br>Hint: If you manually leveled your pet to " + hintLvl + " before placing in the farm, you can save " + Math.max(stage, 1) + " pass";
+        // if (stage > 1 && hintCheck === true) ender += "es";
+
+        const out = "It would take " + result.totalPasses + passType + " pass" + plural + " (" + result.totalPasses / Math.max(1, stage) * 12 + " hours) to level your s" + stage + " pet from " + startLvl + " to " + result.level + ender;
         document.getElementById("result").innerHTML = out;
+    }
+
+    document.getElementById("petDetails").onsubmit = (e) => {
+        e.preventDefault();
+        calculate();
+    };
+
+    document.getElementById("useOnePass").onclick = () => {
+        const next = roundTo(Number(document.getElementById("startLvl").value) + 0.01, 2);
+        document.getElementById("endLvl").value = next;
+        calculate();
     };
 };
